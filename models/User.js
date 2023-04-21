9186e44ef2ff017d45ae1e1481fca8b7b67b4be1
pikiavila <wowaura833@gmail.com>
2023-04-21 15:29:38 -0300

    b-02-v1

---
+++ models/User.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+let schema = new mongoose.Schema({
+  email: {type: String, required: true},
+  password: {type: String, required: true},
+  photo: {type: String, required: true},
+  role: {type: Number, required: true},
+  is_online: {type: Boolean, required: true},
+  is_verified:{type: Boolean, required: true},
+  verifyde_code:{type: String, required: true}
+},{
+  timestamps: true
+})
+
+let collection = 'users' //debe ser SIEMPRE en plural xq es un conjunto de recursos, documentos, etc y en lo posible en minuscula
+let User = mongoose.model(schema, collection)
+
+export default User
---
+++ models/data/categories.js
@@ -0,0 +1,15 @@
+export let categories = [
+    {
+        name: "shonen",
+        detail: "Shonen manga are characterized by having a lot of action and often humorous situations with male protagonists."
+    },{
+        name: "comic",
+        detail: "Marvel, DC and anothers comics"
+    },{
+        name: "shojo",
+        detail: "It is aimed especially at the adolescent female audience, being mostly starring a girl."
+    },{
+        name: "seinen",
+        detail: "Japanese seinen tells stories with a mature tone."
+    }
+]
