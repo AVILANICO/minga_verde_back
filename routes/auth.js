670a827e2d98d51d3859d87626005fa57c88a105
AVILANICO <122701169+AVILANICO@users.noreply.github.com>
2023-04-28 18:47:18 -0300

    M 01 01 (#11)
    * routes-middlewares-v1
    
    * middlewares

---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import create from '../controllers/auth/create.js';
+
+import validator from '../middlewares/validator.js';
+import { userCreateSignUp } from '../schemas/users.js';
+import accountExistsSignUp from '../middlewares/accountSingUp.js';
+
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/signup', validator(userCreateSignUp), accountExistsSignUp, create)
+
+export default router;
