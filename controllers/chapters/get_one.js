ca9042966cec84a93f9bd56eafe6f6a3f3eebd97
Luis Hernandez <lic.luish05@gmail.com>
2023-05-09 08:50:33 -0500

    controlles and routes

--- controllers/chapters/get_one.js
+++ controllers/chapters/get_one.js
@@ -3,7 +3,7 @@ import Chapter from "../../models/Chapter.js";
 let get_one = async (req, res, next) => {
     try {
         
-        let get_one = await Chapter.findOne({_id:req.params.id},'pages -_id' )
+        let get_one = await Chapter.findOne({_id:req.params.id},'pages title order -_id' )
         if (get_one){
             return res.status(200).json({
                 response: get_one
