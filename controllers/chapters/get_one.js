4b690577155caea7aaad75984d9f9420f36697cd
Luis Hernandez <125385157+LuisH05@users.noreply.github.com>
2023-05-11 15:13:50 -0500

    M 09 (#24)
    * M 01 01 (#11)
    
    * routes-middlewares-v1
    
    * middlewares
    
    * M 01 (#12)
    
    * validaciones de datos correctos
    
    * validaciones de usuario
    
    * signout-postman
    
    * M 04 (#13)
    
    * chapter back
    
    * chapter back
    
    * chapter
    
    * creating controlles , routes , middlewares (#14)
    
    * cambio de signup a register (#15)
    
    * M 01 01 (#11)
    
    * routes-middlewares-v1
    
    * middlewares
    
    * M 01 (#12)
    
    * validaciones de datos correctos
    
    * validaciones de usuario
    
    * signout-postman
    
    * M 04 (#13)
    
    * chapter back
    
    * chapter back
    
    * chapter
    
    * creating controlles , routes , middlewares (#14)
    
    * cambio de signup a register
    
    ---------
    
    Co-authored-by: Luis Hernandez <125385157+LuisH05@users.noreply.github.com>
    Co-authored-by: JoelDamato <127883956+JoelDamato@users.noreply.github.com>
    
    * passport (#16)
    
    * passport (#17)
    
    * fixeando ultimos detalles
    
    * respuesta de peticion
    
    * fixed
    
    * controlles and routes
    
    * getOne
    
    * fixed
    
    ---------
    
    Co-authored-by: AVILANICO <122701169+AVILANICO@users.noreply.github.com>
    Co-authored-by: JoelDamato <127883956+JoelDamato@users.noreply.github.com>
    Co-authored-by: pikiavila <wowaura833@gmail.com>

--- controllers/chapters/get_one.js
+++ controllers/chapters/get_one.js
@@ -4,13 +4,18 @@ let get_one = async (req, res, next) => {
     try {
         
         let get_one = await Chapter.findOne({_id:req.params.id},'pages title order -_id' )
+        let id_next = await Chapter.findOne({order:get_one.order + 1}, '_id')
+        let id_prev = await Chapter.findOne({order:get_one.order - 1}, '_id')
         if (get_one){
             return res.status(200).json({
-                response: get_one
+                response: get_one,
+                id_next: id_next?._id || null,
+                id_prev: id_prev?._id || null
             })
         }   return res.status(404).json({
                 response: 'the chapter was not found'
             })
+
     } catch (error) {
         next(error) 
     }
