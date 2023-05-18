079f9b3492392877705966429e5ee4917567cc4a
Luis Hernandez <lic.luish05@gmail.com>
2023-05-18 16:34:38 -0500

    fixed

--- controllers/chapters/update.js
+++ controllers/chapters/update.js
@@ -1,22 +1,26 @@
 import Chapter from "../../models/Chapter.js";
 
 const update = async (req,res,next) => {
+    let select = req.body.key
+    let value = req.body.value
     try {
-        let update = await Chapter.updateOne(
+        let update = await Chapter.findOneAndUpdate(
             {_id: req.params.id}, // objeto de busqueda de lo que se quiere modificar
-            req.body              // objeto con la modificacion (modifica una, dos o todas las propiedades que envie el cliente)
+            {[select]:value },            // objeto con la modificacion (modifica una, dos o todas las propiedades que envie el cliente)
+            {new:true} 
         )
-        if (update.modifiedCount ) {
+        if (update !== null) {
             return res.status(200).json({
                 success: true,
-                message: 'updated',
-            })
-        } else {
+              message: 'Updated',
+              update
+            });
+          } else {
             return res.status(404).json({
-                success: false,
-                message: 'Not found',
-            })
-        }
+              success: false,
+              message: 'Not found'
+            });
+          }
 
     } catch (error) {
         next(error);
