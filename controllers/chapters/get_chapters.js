2e8b1f0aafdfd79888f86716f2fd3f6c89770be4
pikiavila <wowaura833@gmail.com>
2023-06-01 18:22:15 -0300

    ultimo fix (?

--- controllers/chapters/get_chapters.js
+++ controllers/chapters/get_chapters.js
@@ -35,22 +35,10 @@ let get_chapters = async(req, res, next) => { // la funcion controladora debe se
     {
       let all = await Chapter.find(queries, 'title cover_photo order')
       .sort(sort)
-<<<<<<< HEAD
+      
       //agregar un condicional que me indique si "all" esta vacio y responder un "404" 
       .skip(pagination.page > 0 ? (pagination.page-1)*pagination.limit:0)//desde donde pagino 
       .limit(pagination.limit > 0 ? pagination.limit:0)//hasta donde pagino 
-=======
-<<<<<<< HEAD
-      .skip(pagination.page > 0 ? (pagination.page-1)*pagination.limit:0)//desde donde pagino, saltea capitulos
-      .limit(pagination.limit > 0 ? pagination.limit:0)//hasta donde pagino 
-      
-      //agregar un condicional que me indique si "all" esta vacio y responder un "404" 
-=======
-      .skip(pagination.page > 0 ? (pagination.page-1)*pagination.limit:0)//desde donde pagino 
-      .limit(pagination.limit > 0 ? pagination.limit:0)//hasta donde pagino 
-      
->>>>>>> a76dcab429e7e3049128c248c8509948997b6e71
->>>>>>> 96b2055865d7bf034613ea708981944426487e06
       return res.status(200)
       .json({
         succes:true,
@@ -58,11 +46,6 @@ let get_chapters = async(req, res, next) => { // la funcion controladora debe se
         count: count,
         cantPages: cantPages
       })
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
->>>>>>> 96b2055865d7bf034613ea708981944426487e06
     }
   catch(error){
       console.log(error)
@@ -70,16 +53,6 @@ let get_chapters = async(req, res, next) => { // la funcion controladora debe se
     }
       return res.status(400)
       .json({error: "a ocurrido un problema"})
-<<<<<<< HEAD
-=======
-=======
-    }
-  catch(error){
-      console.log(error)
-    }
-      return res.status(400).json({error: "a ocurrido un problema"})
->>>>>>> a76dcab429e7e3049128c248c8509948997b6e71
->>>>>>> 96b2055865d7bf034613ea708981944426487e06
 }
 
 export default get_chapters
